6852d5cd1832159ce6c0ef064404d9c17135968f
Dwi Rasyari Putra <e41230575@student.polije.ac.id>
2024-10-27 23:21:44 +0700

    new

--- app/src/main/java/com/example/ngikngik/lupapassword.java
+++ app/src/main/java/com/example/ngikngik/lupapassword.java
@@ -1,12 +1,28 @@
 package com.example.ngikngik;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class lupapassword extends AppCompatActivity {
 
@@ -19,6 +35,44 @@ public class lupapassword extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        EditText editText = findViewById(R.id.etEmailLupaPw);
+        Button button = findViewById(R.id.lanjutpw);
+        ProgressBar progressBar = findViewById(R.id.progress);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.GONE);
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url ="http://192.168.18.231/db_sabiproject/resetpassword.php";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                if (response.equals("Succes")){
+                                    Intent intent = new Intent(getApplicationContext(), newpasswordpage.class);
+                                    intent.putExtra("email", editText.getText().toString());
+                                    startActivity(intent);
+                                    finish();
+                                } else
+                                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    progressBar.setVisibility(View.GONE);
+                        error.printStackTrace();
+                    }
+                }){
+                    protected Map<String, String> getParams(){
+                        Map<String, String> paramV = new HashMap<>();
+                        paramV.put("param", editText.getText().toString());
+                        return paramV;
+                    }
+                };
+                queue.add(stringRequest);
+            }
         });
     }
 }
--- app/src/main/java/com/example/ngikngik/masukkanOTP.java
+++ app/src/main/java/com/example/ngikngik/masukkanOTP.java
@@ -1,9 +1,6 @@
 package com.example.ngikngik;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,19 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class newpasswordpage extends AppCompatActivity {
+public class masukkanOTP extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_newpasswordpage);
+        setContentView(R.layout.activity_masukkan_otp);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
 }
