a24523177a942567681239b72a556d70b968347d
Dwi Rasyari Putra <e41230575@student.polije.ac.id>
2024-11-06 02:14:48 +0700

    new

--- app/src/main/java/com/example/ngikngik/DbContract.java
+++ app/src/main/java/com/example/ngikngik/DbContract.java
@@ -1,6 +1,6 @@
 package com.example.ngikngik;
 
 public class DbContract {
-    public static final String SERVER_LOGIN_URL = "http://10.10.181.237/db_sabiproject/checklogin.php";
-    public static final String SERVER_REGISTER_URL = "http://10.10.181.237/db_sabiproject/createData.php";
+    public static final String SERVER_LOGIN_URL = "http://192.168.18.231/db_sabiproject/checklogin.php";
+    public static final String SERVER_REGISTER_URL = "http://192.168.18.231/db_sabiproject/createData.php";
 }
