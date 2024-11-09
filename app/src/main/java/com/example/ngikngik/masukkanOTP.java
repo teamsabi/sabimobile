00d3bb8e0ccda235bb612da3601326ab0f50288a
Dwi Rasyari Putra <e41230575@student.polije.ac.id>
2024-11-09 19:05:34 +0700

    new

--- app/src/main/java/com/example/ngikngik/masukkanOTP.java
+++ app/src/main/java/com/example/ngikngik/masukkanOTP.java
@@ -1,14 +1,30 @@
 package com.example.ngikngik;
 
+import static androidx.core.content.ContextCompat.startActivity;
+import static java.security.AccessController.getContext;
+
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -24,32 +40,111 @@ import com.android.volley.toolbox.Volley;
 import java.util.HashMap;
 import java.util.Map;
 
-public class masukkanOTP extends AppCompatActivity {
+public class masukkanOTP extends Dialog {
     Button btnbatal;
     private EditText otp1,otp2,otp3,otp4,otp5,otp6;
-    private Button buttonlanjut;
+    private TextView kirimulang;
+    private Button lanjut;
+
+    //resend otp time is seconds
+    private int resendTime = 60;
+
+    //will be true after 60 seconds
+    private boolean resendEnabled = false;
+    public masukkanOTP(@NonNull Context context) {
+        super(context);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_masukkan_otp);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
         Button btnbatal = findViewById(R.id.batalotp);
         Button buttonlanjut = findViewById(R.id.lanjut);
         ProgressBar progressBar = findViewById(R.id.progressBar2);
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(getContext().getResources().getColor(android.R.color.transparent)));
+        setContentView(R.layout.activity_masukkan_otp);
 
 
-        btnbatal.setOnClickListener(new View.OnClickListener() {
+        otp1 = findViewById(R.id.otp1);
+        otp2 = findViewById(R.id.otp2);
+        otp3 = findViewById(R.id.otp3);
+        otp4 = findViewById(R.id.otp4);
+        otp5 = findViewById(R.id.otp5);
+        otp6 = findViewById(R.id.otp6);
+
+    otp1.addTextChangedListener(textWatcher);
+        otp2.addTextChangedListener(textWatcher);
+        otp3.addTextChangedListener(textWatcher);
+        otp4.addTextChangedListener(textWatcher);
+        otp5.addTextChangedListener(textWatcher);
+        otp6.addTextChangedListener(textWatcher);
+
+        //By default open Keyboard on first EditText
+        showKeyboard(otp1);
+
+        //start countdown timer
+        kirimulang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(masukkanOTP.this, lupapassword.class);
-                startActivity(intent);
+                if(resendEnabled){
+
+                    //resend code here
+                    starCountDownTimer();
+                }
             }
         });
+        lanjut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //handle verif process here
+            }
+        });
+    }
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+    private void showKeyboard(EditText otp){
+        otp.requestFocus();
+
+        InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.showSoftInput(otp, InputMethodManager.SHOW_IMPLICIT);
+    }
+    private void starCountDownTimer() {
+        resendEnabled = false;
+        kirimulang.setTextColor(Color.parseColor("#99000000"));
+
+        new CountDownTimer(resendTime * 1000, 1000){
+
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                kirimulang.setText("Kirim Ulang ("+(millisUntilFinished / 1000 )+")");
+            }
+
+            @Override
+            public void onFinish() {
+                resendEnabled = true;
+                kirimulang.setText("Kirim Ulang");
+                kirimulang.setTextColor(getContext().getResources().getColor(android.R.color.holo_blue_dark));
+            }
+        }.start();
     }
 }
