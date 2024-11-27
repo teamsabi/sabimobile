fca6c48387094ef2bef734e4d4f8a06a45f0ce3e
Dwi Rasyari Putra <e41230575@student.polije.ac.id>
2024-11-27 21:57:31 +0700

    update

--- app/src/main/java/com/example/ngikngik/Dashboard/edit_profil/profil_edit.java
+++ app/src/main/java/com/example/ngikngik/Dashboard/edit_profil/profil_edit.java
@@ -1,5 +1,8 @@
 package com.example.ngikngik.Dashboard.edit_profil;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,20 +15,34 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.ngikngik.Dashboard.Jadwal;
 import com.example.ngikngik.Dashboard.JadwalAdapter;
+import com.example.ngikngik.Dashboard.ProfilDashboard;
 import com.example.ngikngik.R;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class profil_edit extends Fragment {
+    private ImageView imageView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profil_edit, container, false);
         Log.d("DEBUG", "profil_edit fragment loaded.");
-        return inflater.inflate(R.layout.fragment_profil_edit, container, false);
+
+        imageView = view.findViewById(R.id.imgBack);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ProfilDashboard.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+
 
     }
   }
