354d0ab32d902a9526b3e00eab8183d0f9ad212f
Dwi Rasyari Putra <e41230575@student.polije.ac.id>
2024-12-25 21:45:51 +0700

    update masukkan otp

--- app/src/main/java/com/example/ngikngik/api/DbContract.java
+++ app/src/main/java/com/example/ngikngik/api/DbContract.java
@@ -1,15 +1,15 @@
 package com.example.ngikngik.api;
 
 public class DbContract {
-    static final String IP = "192.168.18.231";
+//    static final String IP = "192.168.18.231";
 //    static final String IP = "192.168.1.11";
-//    static final String IP = "192.168.1.12";
+    static final String IP = "192.168.1.12";
     public static final String SERVER_LOGIN_URL = "http://" + IP + "/api/apilogin.php";
     public static final String SERVER_REGISTER_URL = "http://" + IP + "/api/apiregister.php";
     public static final String SERVER_LUPA_PASSWORD_URL = "http://" + IP + "/api/lupapassword.php";
     public static final String SERVER_VERIF_OTP_URL = "http://" + IP + "/api/verifotp.php";
     public static final String SERVER_NEWPASSWORD_URL= "http://" + IP + "/api/newpassword.php";
-    public static final String SERVER_RESEND_OTP_URL = "http://" + IP + "/db_sabiproject/resend_otp.php";
+    public static final String SERVER_RESEND_OTP_URL = "http://" + IP + "/api/resendotp.php";
     public static final String SERVER_JADWAL_URL = "http://" + IP + "/db_sabiproject/JadwalKelas.php";
     public static final String SERVER_EDIT_PROFIL_URL = "http://" + IP + "/db_sabiproject/editprofil.php";
     public static final String SERVER_NAMA_URL = "http://" + IP + "/db_sabiproject/namaAPI.php";
